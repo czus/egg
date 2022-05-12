5c14e7691dac78553f76fa611b8ac507f5a39115
czusx <czus.xx@gmail.com>
2022-05-12 11:06:51 -0300

    reverting things using python 3.8 and some recursion...

---
+++ revert.py
@@ -0,0 +1,16 @@
+"""How to do an inversion of any kind of input using recursion..."""
+
+
+def rev(string):
+    string_type = type(string)
+    empty_string = string_type()
+    if string == empty_string:
+        return empty_string
+    rest = rev(string[1:]) #recursive magic
+    first_string = string[0:1]
+    result = rest + first_string
+
+    return result
+
+
+print(rev("string"))
